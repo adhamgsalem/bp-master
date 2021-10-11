11249247d7074182ea04de13d42909498bf70fe0
Adham Salem <adham@metalogic.ie>
2021-10-11 21:11:11 +0100

    Created a new feature1 branch

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -6,7 +6,7 @@ namespace BPCalculator
     // BP categories
     public enum BPCategory
     {
-        [Display(Name = "Nothing")] None,
+        [Display(Name = "Error, Please measure again!")] None,
         [Display(Name="Low Blood Pressure")] Low,
         [Display(Name="Ideal Blood Pressure")]  Ideal,
         [Display(Name="Pre-High Blood Pressure")] PreHigh,
